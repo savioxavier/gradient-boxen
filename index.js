8ef3195d107908881e28cc7b4f6be4539fe4f44c
Skyascii <savioxavier112@gmail.com>
2022-11-01 20:11:17 +0400

    style: format using prettier
    also tabs to spaces as vscode-prettier now uses spaces as the standard

--- index.js
+++ index.js
@@ -5,67 +5,67 @@ import stripAnsi from 'strip-ansi';
 import { stripIndents } from 'common-tags';
 
 const ALLOWED_GRADIENTS = [
-	'cristal',
-	'teen',
-	'mind',
-	'morning',
-	'vice',
-	'passion',
-	'fruit',
-	'instagram',
-	'atlas',
-	'retro',
-	'summer',
-	'pastel',
-	'rainbow',
+  'cristal',
+  'teen',
+  'mind',
+  'morning',
+  'vice',
+  'passion',
+  'fruit',
+  'instagram',
+  'atlas',
+  'retro',
+  'summer',
+  'pastel',
+  'rainbow',
 ];
 
 // Type checking in JS is a mess.
 // Warning: real bad code ahead
 function handleAllErrors(boxText, boxOptions, gradientOptions) {
-	if (typeof boxText !== 'string') {
-		throw new Error('No box text provided');
-	}
+  if (typeof boxText !== 'string') {
+    throw new Error('No box text provided');
+  }
 
-	if (!boxOptions) {
-		throw new Error('No box options provided');
-	}
+  if (!boxOptions) {
+    throw new Error('No box options provided');
+  }
 
-	if (Object.prototype.toString.call(boxOptions) !== '[object Object]') {
-		throw new Error('Box options must be an object');
-	}
+  if (Object.prototype.toString.call(boxOptions) !== '[object Object]') {
+    throw new Error('Box options must be an object');
+  }
 
-	if (!gradientOptions) {
-		throw new Error('No gradient specified');
-	}
+  if (!gradientOptions) {
+    throw new Error('No gradient specified');
+  }
 
-	if (gradientOptions) {
-		if (
-			!(
-				typeof gradientOptions === 'string' ||
-				(Array.isArray(gradientOptions) &&
-					gradientOptions.every(
-						(gradientOption) => typeof gradientOption === 'string'
-					))
-			)
-		) {
-			throw new TypeError(
-				stripIndents`Gradient must be a string or an array of valid color codes
+  if (gradientOptions) {
+    if (
+      !(
+        typeof gradientOptions === 'string' ||
+        (Array.isArray(gradientOptions) &&
+          gradientOptions.every(
+            (gradientOption) => typeof gradientOption === 'string'
+          ))
+      )
+    ) {
+      throw new TypeError(
+        stripIndents`Gradient must be a string or an array of valid color codes
         Received type: ${typeof gradientOptions}. Expected type: 'string' or 'array'`
-			);
-		}
+      );
+    }
 
-		if (
-			typeof gradientOptions === 'string' &&
-			!ALLOWED_GRADIENTS.includes(gradientOptions)
-		) {
-			throw new Error(
-				stripIndents`Gradient name must be one of the following: ${ALLOWED_GRADIENTS.join(
-					', '
-				)}. Received gradient name: '${gradientOptions}'`
-			);
-		}
-	}
+    if (
+      typeof gradientOptions === 'string' &&
+      !ALLOWED_GRADIENTS.includes(gradientOptions)
+    ) {
+      throw new Error(
+        stripIndents`Gradient name must be one of the following: ${ALLOWED_GRADIENTS.join(
+          ', '
+        )}. Received gradient name: '${gradientOptions}'`
+      );
+    }
+  }
 }
 
 // eslint-disable-next-line consistent-return
@@ -87,25 +87,29 @@ function handleAllErrors(boxText, boxOptions, gradientOptions) {
  *  })
  * );
  */
-function gradientBox(boxText, boxOptions={borderStyle:'round', padding:0.75, marging:0.75}, gradientOptions=['#11998e', '#38ef7d']) {
-	handleAllErrors(boxText, boxOptions, gradientOptions);
+function gradientBox(
+  boxText,
+  boxOptions = { borderStyle: 'round', padding: 0.75, marging: 0.75 },
+  gradientOptions = ['#11998e', '#38ef7d']
+) {
+  handleAllErrors(boxText, boxOptions, gradientOptions);
 
-	if (typeof gradientOptions === 'string') {
-		const box = gradient[gradientOptions].multiline(
-			stripAnsi(boxen(boxText, boxOptions))
-		);
+  if (typeof gradientOptions === 'string') {
+    const box = gradient[gradientOptions].multiline(
+      stripAnsi(boxen(boxText, boxOptions))
+    );
 
-		return `${box}\u001b[0m`;
-	}
-	if (gradientOptions instanceof Array) {
-		const box = gradient([...gradientOptions]).multiline(
-			stripAnsi(boxen(boxText, boxOptions))
-		);
+    return `${box}\u001b[0m`;
+  }
+  if (gradientOptions instanceof Array) {
+    const box = gradient([...gradientOptions]).multiline(
+      stripAnsi(boxen(boxText, boxOptions))
+    );
 
-		return `${box}\u001b[0m`;
-	}
+    return `${box}\u001b[0m`;
+  }
 
-	return '';
+  return '';
 }
 
 export default gradientBox;
